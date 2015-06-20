361facf4032a8c745bb34ceabc8f8bf3e9e267fa
step <stepets@ua.fm>
2015-06-20 19:11:59 +0300

    Add test project
    test

---
+++ ballVSsquare/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour {
+
+	GameObject playerPos;
+	Vector3 offset;
+	// Use this for initialization
+	void Start () {
+
+		playerPos = GameObject.FindWithTag ("Player");
+		offset = new Vector3 (5, 8, -4);
+//		offset.transform.rotation.Set(30, 350,0,1);
+
+		this.transform.rotation.Set (50, 350, 0, 1);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+	void LateUpdate()
+	{
+		this.transform.position = offset + playerPos.transform.position;
+
+
+
+	}
+}
